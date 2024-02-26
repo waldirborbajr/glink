6706eb0cf3d19baeb5cf917c0a88a0177c9cebc2
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-02-26 13:20:53 -0300

    feat: version

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/waldirborbajr/glink/glinkcli"
 )
 
-var version = "dev"
+var (
+	version  = "0.1.0"
+	revision = "dev"
+)
 
 func main() {
-	app := glinkcli.App(version)
+	app := glinkcli.App(version, revision)
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("glink v 0.1.1")
 }
